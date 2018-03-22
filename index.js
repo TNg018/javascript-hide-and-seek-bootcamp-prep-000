9d388fbb5d5217157f1d3f6f1334c3b07cfdb019
Tien Nguyen <pri859@sudomail.com>
2018-03-22 01:45:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+return  document.querySelector(selector)
+
+}
+
+function nestedTarget() {
+return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+var ranks = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var deepestChild = document.getElementById('grand-node').querySelectorAll('div')
+  return deepestChild[deepestChild.length - 1]
+}
